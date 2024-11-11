189e28e5944ef6a823aba6c3b5c20d543dd42408
arindam bhattacharyya <arindambhattacharyya.ab@gmail.com>
2024-11-11 16:40:03 +0530

    before implementing  hero widget

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -5,193 +5,175 @@ import 'package:travel_app/provider/model.dart';
 import '../home_Page/special_for_you_text.dart';
 import './book_mark_card.dart';
 import '../home_Page/prime_location_card.dart';
-
 import 'text_button_control_card.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-
-    //* Retrieve the filtered list based on the selected filter
     List<Map<String, dynamic>> filteredData =
         context.watch<HomePageProvider>().getFilteredTravelList();
-
     int lengthOfData = filteredData.length;
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          automaticallyImplyLeading: false,
-          elevation: 0,
-          title: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
+    Widget appBarContent() {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage(
+                'assets/images/profile_picture.jpg',
+              ),
+            ),
+            SizedBox(width: 10),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/images/profile_picture.jpg',
+                Text(
+                  'Jihan Audy,',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Jihan Audy,',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Where do you want to go?',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ],
-                ),
-                Spacer(),
-                Icon(
-                  Icons.notification_add_outlined,
-                  color: Colors.black,
+                Text(
+                  'Where do you want to go?',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
                 ),
               ],
             ),
-          )),
-      body: Column(
-        children: [
-          //* Search bar
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                filled: true,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                fillColor: Colors.white,
-                hintText: 'Search',
-                prefixIcon: Icon(Icons.search),
-                suffixIcon: Icon(Icons.tune_outlined),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.green),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-              ),
+            Spacer(),
+            Icon(
+              Icons.notification_add_outlined,
+              color: Colors.black,
             ),
-          ),
-          SizedBox(height: 20),
-          //*  4 button to navigation cards
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Row(
-                children: [
-                  TextButtonControlCard(
-                    id: 0,
-                    buttonText: allButtonText.All.name,
-                  ),
-                  SizedBox(width: 8),
-                  TextButtonControlCard(
-                    id: 1,
-                    buttonText: allButtonText.Popular.name,
-                  ),
-                  SizedBox(width: 8),
-                  TextButtonControlCard(
-                    id: 2,
-                    buttonText: allButtonText.Recommended.name,
-                  ),
-                  SizedBox(width: 8),
-                  TextButtonControlCard(
-                    id: 3,
-                    buttonText: allButtonText.WishListed.name,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 300.0,
-                    //* Creating the cards
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: lengthOfData,
-                      itemBuilder: (context, index) {
-                        //* in CardData we assign filterData with specific index
-                        final cardData =
-                            filteredData[index]; //* index ->| 0 | 1 | 2 | 3 |
-                        int id = cardData['id'];
-                        String urlImage = cardData['image'];
-                        bool isAddedToWishList =
-                            cardData['isUserWishListedValue'];
-                        String destination = cardData['location'];
-                        String destinationState = cardData['state'];
-                        String destinationCountry = cardData['country'];
+          ],
+        ),
+      );
+    }
 
-                        return Padding(
-                          padding: const EdgeInsets.only(
-                              left: 16.0), //for single card shifting
-                          child: BookMarkCard(
-                            id: id,
-                            image: urlImage,
-                            bookMark: isAddedToWishList,
-                            location: destination,
-                            state: destinationState,
-                            country: destinationCountry,
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  SpecialForYouText(),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              elevation: 0,
+              //* pinned: true,  to make the app bar pinned
+              expandedHeight: 120.0,
+              flexibleSpace: FlexibleSpaceBar(
+                collapseMode: CollapseMode.pin,
+                background: appBarContent(),
+              ),
+            ),
+
+            ///////////////
+            SliverPersistentHeader(
+              pinned: true,
+              delegate: _SearchBarDelegate(),
+            ),
+            //////////////////
+
+            SliverList(
+              delegate: SliverChildListDelegate([
+                //* 4 button navigation cards
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
                     child: Row(
                       children: [
-                        Padding(
-                          padding: const EdgeInsets.only(left: 16.0),
-                          child: PrimeLocationCard(),
+                        TextButtonControlCard(
+                          id: 0,
+                          buttonText: allButtonText.All.name,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: PrimeLocationCard(),
+                        SizedBox(width: 8),
+                        TextButtonControlCard(
+                          id: 1,
+                          buttonText: allButtonText.Popular.name,
+                        ),
+                        SizedBox(width: 8),
+                        TextButtonControlCard(
+                          id: 2,
+                          buttonText: allButtonText.Recommended.name,
+                        ),
+                        SizedBox(width: 8),
+                        TextButtonControlCard(
+                          id: 3,
+                          buttonText: allButtonText.WishListed.name,
                         ),
                       ],
                     ),
                   ),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                  SizedBox(height: 100),
-                ],
-              ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  height: 300.0,
+                  //* Creating the cards
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: lengthOfData,
+                    itemBuilder: (context, index) {
+                      final cardData = filteredData[index];
+                      int id = cardData['id'];
+                      String urlImage = cardData['image'];
+                      bool isAddedToWishList =
+                          cardData['isUserWishListedValue'];
+                      String destination = cardData['location'];
+                      String destinationState = cardData['state'];
+                      String destinationCountry = cardData['country'];
+
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: BookMarkCard(
+                          id: id,
+                          image: urlImage,
+                          bookMark: isAddedToWishList,
+                          location: destination,
+                          state: destinationState,
+                          country: destinationCountry,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                SpecialForYouText(),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: PrimeLocationCard(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: PrimeLocationCard(),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 800), // to allow scrolling
+              ]),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
+
+      ///////////////////
       floatingActionButton: Padding(
         padding: const EdgeInsets.all(20.0),
         child: FloatingActionButton.extended(
@@ -239,4 +221,44 @@ class HomePage extends StatelessWidget {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
+}
+
+class _SearchBarDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: TextField(
+        decoration: InputDecoration(
+          filled: true,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          fillColor: Colors.white,
+          hintText: 'Search',
+          prefixIcon: Icon(Icons.search),
+          suffixIcon: Icon(Icons.tune_outlined),
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.green),
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 60.0;
+  @override
+  double get minExtent => 60.0;
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) =>
+      false;
 }
