4a7ac186b103bba53f70de342907c17addc18ea5
arindam bhattacharyya <arindambhattacharyya.ab@gmail.com>
2024-11-15 17:30:25 +0530

    search box dummy container

--- lib/features/home_page/interface/widgets/search_bar.dart
+++ lib/features/home_page/interface/widgets/search_bar.dart
@@ -1,77 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:animations/animations.dart';
 
 class SearchBarContainer extends SliverPersistentHeaderDelegate {
   @override
   Widget build(
       BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return OpenContainer(
-      closedElevation: 0,
-      openElevation: 0,
-      openColor: Colors.red,
-      transitionType: ContainerTransitionType.fadeThrough,
-      //* when it's closed
-      closedBuilder: (context, openContainer) => GestureDetector(
-        onTap: openContainer, // Opens the container when tapped
-        child: Container(
-          alignment: Alignment.center,
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(12),
           color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.black),
-              borderRadius: BorderRadius.circular(12),
-              color: Colors.white,
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.search, size: 30, color: Colors.grey),
-                SizedBox(width: 8),
-                Text(
-                  'Search', // Placeholder text
-                  style: TextStyle(color: Colors.grey, fontSize: 16),
-                ),
-                Spacer(),
-                Icon(Icons.tune_outlined, size: 20, color: Colors.grey),
-              ],
-            ),
-          ),
         ),
-      ),
-      //* when it's open
-      openBuilder: (context, closeContainer) => Scaffold(
-        body: Column(
+        child: Row(
           children: [
-            Container(
-              color: Colors.transparent,
-              height: 100,
+            Icon(Icons.search, size: 30, color: Colors.grey),
+            SizedBox(width: 8),
+            Text(
+              'Search',
+              style: TextStyle(color: Colors.grey, fontSize: 16),
             ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                autofocus:
-                    true, // Focus the TextField and open the keyboard here
-                decoration: InputDecoration(
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.teal[400]!),
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.black),
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                  fillColor: Colors.white,
-                  hintText: 'Search',
-                  prefixIcon: Icon(Icons.search, size: 30),
-                  suffixIcon: Icon(Icons.tune_outlined, size: 20),
-                ),
-              ),
-            ),
+            Spacer(),
+            Icon(Icons.tune_outlined, size: 20, color: Colors.grey),
           ],
         ),
       ),
-      transitionDuration: Duration(milliseconds: 800),
     );
   }
 
